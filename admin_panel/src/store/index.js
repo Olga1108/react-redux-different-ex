c2088b3f3173613fe0866b425a2bddd707fae909
Olha Pushkar <Olha_Pushkar@epam.com>
2021-11-24 14:27:01 +0300

    add: redux-thunk, middleware

--- admin_panel/src/store/index.js
+++ admin_panel/src/store/index.js
@@ -1,14 +1,21 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+};
 
-// const enhancer = (createStore) => (...args) => {
-//     const store = createStore(...args);
+const store = createStore( 
+                combineReducers({heroes, filters}),
+                compose(applyMiddleware(ReduxThunk, stringMiddleware),
+                    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
-//     const oldDispatch = store.dispatch
-// }
-const store = createStore( combineReducers({heroes, filters}),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
